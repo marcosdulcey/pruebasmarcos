ea5191608790b95f16af31ffb701118cf4c13014
Ing. Marcos Dulcey <marcosdulcey@gmail.com>
2022-01-23 14:46:00 -0400

    Comentario de proyecto nuevo

--- src/test/java/base/basetests.java
+++ src/test/java/base/basetests.java
@@ -1,11 +1,15 @@
 package base;
 
+import org.junit.After;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class basetests{
     private WebDriver driver;
+
     public void setUp(){
         System.setProperty("webdriver.chrome.driver","resources/chromedriver.exe");
         driver = new ChromeDriver();
@@ -16,7 +20,22 @@ public class basetests{
     public void Open(){
         setUp();
 
-    }
+        By elemento = By.className("nav-search-input");
+                      By.className("nav-search-btn");
+        By elementos = By.className("nav-search-btn");
+                        By.className("andes-dropdown__trigger");
+
+    WebElement element = driver.findElement(elemento);
+
+        driver.findElement(elemento).click();
+        driver.findElement(elemento).sendKeys("Rompecabezas");
+        driver.findElement(elementos).click();
+
+}
+@After
+public void close(){
+        driver.quit();
+}
 
 
 }
